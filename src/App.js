0a2470e29a69727327bab56e14260d90c26c3096
Niharreddy <gniharreddy99@gmail.com>
2024-06-14 18:44:03 -0400

    correct error in app.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nihar Reddy Gopidi
+          learn react
         </a>
       </header>
     </div>
